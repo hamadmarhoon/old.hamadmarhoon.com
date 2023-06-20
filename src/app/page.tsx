20143503f47b98ed5a3d3c6ee88be9a648a5c5a3
Hamad <marhoon@email.arizona.edu>
2023-06-20 20:32:04 +0300

    Fixing scroll issue with Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Navbar />
         <Welcome />
         <PortfolioGallery />
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </ParallaxProvider>
   );
