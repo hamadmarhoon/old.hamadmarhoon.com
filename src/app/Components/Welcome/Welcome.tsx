072da13898d0562568e4be85b1b55dafb293d18e
Hamad <marhoon@email.arizona.edu>
2023-07-03 12:46:31 +0300

    Mobile image fix

--- src/app/Components/Welcome/Welcome.tsx
+++ src/app/Components/Welcome/Welcome.tsx
@@ -3,7 +3,7 @@
 import Grid from "@mui/material/Grid";
 import "./Welcome.css";
 import Image from "next/image";
-import Headshot from "../../Images/headshot.jpeg";
+import Headshot from "../../Images/headshot.jpg";
 
 export default function Welcome() {
   return (
